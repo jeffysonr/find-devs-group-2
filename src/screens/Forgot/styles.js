06f1e7b0320e54260e152f5e9cdc6683fcaa711c
andre <andre.decarvalho1@gmail.com>
2022-09-14 17:40:55 -0300

    update styles ARN-22

--- src/screens/Forgot/styles.js
+++ src/screens/Forgot/styles.js
@@ -2,40 +2,42 @@ import { StyleSheet, Dimensions, } from 'react-native'
 
 export default StyleSheet.create({
     container: {
-        backgroundColor: '#121214',
+        backgroundColor: '#202024',
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
         alignItems: 'center',
-        paddingVertical: 40,
+        paddingVertical:20,
         flex: 1,
     },
 
     form: {
-        marginTop: 80,
-        marginBottom: 120,
+        marginTop: 20,
+        marginBottom: 100,
     },
     inputContainer:{
-        flexDirection:'row'
+        flexDirection:'row',
+        textAlign: 'center',
+        justifyContent: 'center',
+        marginLeft: -20
     },
     inputs: {
-        backgroundColor: 'black',
+        backgroundColor: '#121214',
         color: 'white',
-        padding: 10,
-        width: Dimensions.get('window').width - 80,
+        paddingVertical: 10,
+        width: '90%',
         marginBottom: 20,
-        borderRadius: 5,
+        borderRadius: 6,
         fontFamily: 'Poppins_400Regular',
         fontSize: 12,
-        paddingLeft: 45,
-        translateX: -10,
-    },
+        padding: 50,
+},
     title: {
         fontFamily: 'Poppins_400Regular',
         fontSize: 20,
         color: 'white',
         marginBottom: 10,
         alignSelf: 'flex-start',
-        paddingLeft: 40,
+        paddingLeft: 20,
 
     },
     texts: {
@@ -45,28 +47,21 @@ export default StyleSheet.create({
         fontSize: 12,
         textAlign: 'center'
     },
-    textLink: {
-        textDecorationLine: 'underline',
-        fontFamily: 'Poppins_400Regular',
-        color: 'white',
-        margin: 20,
-        fontSize: 12,
-        textAlign: 'center'
-    },
-    
+
     buttons: {
-        width: '80%',
-        height: 60,
+
+        width: '90%',
+        height: 56,
         textAlign: 'center',
-        justifyContent: 'center',
+        justifyContent:'center',
         backgroundColor: '#2D9135',
-        borderRadius: 10,
-        marginBottom: 20
+        borderRadius: 6,
+
     },
-        
+
     icon:{
-        translateX: 25,
+        translateX: 35,
         translateY: 10,
         zIndex: 2,
-    }
+    },
 })
